5902b35361f2c3b47042bd44bc9253741cdda16d
Davor Altman <davor.altman@gmail.com>
2016-09-19 23:01:37 -0700

    initial commit

---
+++ goku.js
@@ -0,0 +1,66 @@
+var GoGoku = 0;
+var addMarginTop = 20;
+var addMarginLeft = 20;
+
+	
+function gokuGoDown() {
+
+	//check which key is pressed
+	var x = event.keyCode;
+		
+	//only move goku if keyboard arrows are used
+	
+	if (x == 40) {
+
+		GoGoku = document.getElementById("goku").style.marginTop = addMarginTop + "px";
+		addMarginTop += 20;
+
+	} else if (x == 39) {
+		
+		GoGoku = document.getElementById("goku").style.marginLeft = addMarginLeft + "px";
+		addMarginLeft += 20;	
+
+	} else if (x == 38) {
+
+		GoGoku = document.getElementById("goku").style.marginTop = addMarginTop + "px";
+		addMarginTop -= 20;
+
+	} else if (x == 37) {
+
+		GoGoku = document.getElementById("goku").style.marginLeft = addMarginLeft + "px";
+		addMarginLeft -= 20;
+
+	}
+
+	return GoGoku;
+
+}
+
+//make the rock float
+
+function floatRock() {
+
+	var moveRock = setTimeout(function() {
+		document.getElementById("obstacle").style.marginRight = 110 + "%";
+		}, 500 );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
