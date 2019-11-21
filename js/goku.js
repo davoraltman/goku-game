ca3d63d81db139aa04e1a91c2577ec7d8e318fa6
Davor Altman <davor.altman@gmail.com>
2019-11-21 20:01:40 +0100

    dodao sam jedan komentar

--- js/goku.js
+++ js/goku.js
@@ -58,7 +58,7 @@ var overlap = !(rect1.right < rect2.left ||
                 rect1.left > rect2.right ||
                 rect1.bottom < rect2.top ||
                 rect1.top > rect2.bottom) */
-
+//novi komentar
 //add timer
 var i = 0;
 
